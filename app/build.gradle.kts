88eb53686c1fe98d04fd14a60c9adc101fe862f3
if <1096005725@qq.com>
2026-09-06 23:13:24 +0800

    v1.0.49: 菜单卡片进一步收窄至内容宽度

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.today.habit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 49
-        versionName = "1.0.48"
+        versionCode = 50
+        versionName = "1.0.49"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
